8b31027b12804ea8944ca41badb4e32b3a7a0809
Muhammad-Yunus <muhammadyunusiot@gmail.com>
2020-10-24 12:14:10 +0700

    update support aarch64 && support https

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM node:8.3.0-stretch
-MAINTAINER Rolf Larsen
+FROM aarch64/node:4.6.1
 
 RUN git clone https://github.com/rhodiumlabs/node-mjpeg-proxy.git \
 #  && cd node-mjpeg-proxy \
   && npm install express \
-  && npm install mjpeg-proxy
+  && npm install mjpeg-proxy \
+  && npm install https \
+  && npm install fs
 
 #
 # Define an environment variable
@@ -12,7 +13,11 @@ RUN git clone https://github.com/rhodiumlabs/node-mjpeg-proxy.git \
 # Use this variable when creating a container to specify the MJPEG host.
 ENV MJPEG_URL=""
 ENV MJPEG_PORT="8081"
+ENV MJPEG_CONSUMER=""
 
-ADD index.js /
+#ADD index.js /
+ADD . /
+
+RUN chmod +x index.js
 
 CMD nodejs index.js
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 var app = express();
 var mjpegurl = process.env.MJPEG_URL;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/index.jpg', new MjpegProxy(mjpegurl).proxyRequest);
-app.listen(process.env.MJPEG_PORT);
+//app.listen(process.env.MJPEG_PORT);
+
+https.createServer({
+  key: fs.readFileSync('CERT/key.pem'),
+  cert: fs.readFileSync('CERT/cert.pem')
+}, app)
+.listen(process.env.MJPEG_PORT);
